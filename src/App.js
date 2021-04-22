1b3aff5efd2bdfd356e65fa4128177ebd9766be6
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-21 17:32:49 -0700

    if api not reached yet, load spinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import Spinner from "react-bootstrap/Spinner";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
-import "jquery/dist/jquery.slim"
-import "font-awesome/css/font-awesome.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "jquery/dist/jquery.slim";
+import "font-awesome/css/font-awesome.min.css";
 import UserCards from "./components/UserCards";
 import API from "./utils/API";
 
 class App extends Component {
   state = {
-    employees: [],
+    employees: null,
   };
 
   componentDidMount() {
@@ -20,11 +21,18 @@ class App extends Component {
     this.setState({ employees: data.results });
   };
   render() {
-    return (
-      <>
-        <UserCards employees={this.state.employees} />
-      </>
-    );
+    if (this.state.employees === null) {
+      return (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      );
+    } else
+      return (
+        <>
+          <UserCards employees={this.state.employees} />
+        </>
+      );
   }
 }
 

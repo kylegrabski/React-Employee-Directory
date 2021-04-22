19f1b673b4b91cf8ea6fa2d4e7ab9b82b0f8e73d
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-21 17:55:52 -0700

    map all employees

--- src/components/UserCards.jsx
+++ src/components/UserCards.jsx
@@ -1,49 +1,56 @@
 import React from "react";
+import "./style.css";
 
 function UserCards({ employees }) {
-
+  console.log(employees[0]);
   return (
     <>
+    <div className="container py-5">
       {/* <p>NAME: {employees[0].name.first}</p> */}
-
       <div className="row pb-5 mb-4">
-        <div className="col-lg-3 col-md-6 mb-4 mb-lg-0">
-          <div className="card shadow-sm border-0 rounded">
-            <div className="card-body p-0">
-              <img
-                src="https://res.cloudinary.com/mhmd/image/upload/v1570799922/profile-1_dewapk.jpg"
-                alt=""
-                className="w-100 card-img-top"
-              />
-              <div className="p-4">
-                <h5 className="mb-0">Mark Rockwell</h5>
-                <p className="small text-muted">CEO - Consultant</p>
-                <ul className="social mb-0 list-inline mt-3">
-                  <li className="list-inline-item m-0">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-facebook-f"></i>
-                    </a>
-                  </li>
-                  <li className="list-inline-item m-0">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li className="list-inline-item m-0">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-instagram"></i>
-                    </a>
-                  </li>
-                  <li className="list-inline-item m-0">
-                    <a href="#" className="social-link">
-                      <i className="fa fa-linkedin"></i>
-                    </a>
-                  </li>
-                </ul>
+      {employees.map((item) => {
+        return (
+            <div className="col-lg-3 col-md-6 mb-4 mb-lg-0">
+              <div className="card shadow-sm border-0 rounded mb-4">
+                <div className="card-body p-0">
+                  <img
+                    src={item.picture.large}
+                    alt={item.name.first}
+                    className="w-100 card-img-top"
+                  />
+                  <div className="p-4">
+                    <h5 className="mb-0">{item.name.first} {item.name.last}</h5>
+                    <p className="small text-muted">Office - {item.phone}</p>
+                    <p className="small text-muted">Mobile - {item.cell}</p>
+                    <ul className="social mb-0 list-inline mt-3">
+                      <li className="list-inline-item m-0">
+                        <a href="#" className="social-link">
+                          <i className="fa fa-facebook-f"></i>
+                        </a>
+                      </li>
+                      <li className="list-inline-item m-0">
+                        <a href="#" className="social-link">
+                          <i className="fa fa-twitter"></i>
+                        </a>
+                      </li>
+                      <li className="list-inline-item m-0">
+                        <a href="#" className="social-link">
+                          <i className="fa fa-instagram"></i>
+                        </a>
+                      </li>
+                      <li className="list-inline-item m-0">
+                        <a href="#" className="social-link">
+                          <i className="fa fa-linkedin"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+        );
+      })}
+      </div>
       </div>
     </>
   );

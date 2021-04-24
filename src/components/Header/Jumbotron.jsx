ec9f0dec9c006101faeb0af2666d59eef2f4c2a3
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-23 18:54:21 -0700

    added text and sort button

--- src/components/Header/Jumbotron.jsx
+++ src/components/Header/Jumbotron.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { Jumbotron, Button } from "react-bootstrap";
-
-function JumbotronComp() {
+import "./customJumbotron.css";
+function JumbotronComp({handleSortByName}) {
   return (
     <>
       <div className="customJumbotron">
         <div className="container">
           <Jumbotron>
-            <h1>HERES THE JUMBTRON</h1>
-            <p>We can talk about what the app does for the company here</p>
-            <Button variant="dark">View All Employees</Button>
+            <h1>TechLink Inc. Employee Directory</h1>
+            <p>
+              Here is a sample directory from a fictitious company named
+              TechLink Inc!
+            </p>
+
+            <p>Search for an employee using the search bar above</p>
+            <Button variant="dark">Load New Employees</Button>
+         
+            <Button variant="dark"
+            onClick={handleSortByName}
+            >Sort By Name</Button>
           </Jumbotron>
         </div>
       </div>
